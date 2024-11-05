04a4033aa161ba631f5d29d374d1f14394986207
NewBean0312 <newbean0312@gmail.com>
2024-11-05 22:45:32 +0900

    5.가위바위보(미완)

---
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    
+  },
+  computed: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+  #computer {
+    width: 142px;
+    height: 200px;
+    background-position: 0 0;
+  }
+</style>
