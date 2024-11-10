d03d7bac099f359df83fe616e806a08eb4c40102
NewBean0312 <newbean0312@gmail.com>
2024-11-10 20:24:06 +0900

    5.가위바위보(:class와 :style)

--- 5.가위바위보/RockScissorsPaper.vue
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -1,26 +1,53 @@
 <template>
   <div>
+    <div>
+      <div
+        id="computer"
+        :style="computedStyleObject"
+      ></div>
+      <dutton @click="onClickButton('바위')">바위</dutton>
+      <dutton @click="onClickButton('가위')">가위</dutton>
+      <dutton @click="onClickButton('보')">보</dutton>
+    </div>
+    <div>
+      <div>{{ result }}</div>
+      <div>현재 {{ score }}점</div>
+    </div>
   </div>
 </template>
 
 <script>
+const rspCords = {
+  바위: "0",
+  가위: "-142px",
+  보: "-284px",
+};
+
 export default {
   data() {
-    
+    return {
+      imageCode: rspCords.바위,
+      result: "",
+      score: 0,
+    };
   },
   computed: {
-    
+    computedStyleObject() {
+      return {
+        background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imageCode} 0`,
+      }
+    }
   },
   methods: {
-    
+    onClickButton(choice) {},
   },
 };
 </script>
 
 <style scoped>
-  #computer {
-    width: 142px;
-    height: 200px;
-    background-position: 0 0;
-  }
+#computer {
+  width: 142px;
+  height: 200px;
+  background-position: 0 0;
+}
 </style>
