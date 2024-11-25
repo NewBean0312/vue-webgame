a9938c86d79900f7e307c804bdcbc3c3dd65792b
NewBean0312 <newbean0312@gmail.com>
2024-11-25 23:24:51 +0900

    5.가위바위보(가위바위보 완성과 devtools 분석)

--- 5.가위바위보/RockScissorsPaper.vue
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -20,8 +20,27 @@ const rspCords = {
   보: "-284px",
 };
 
-let interval = null;
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
 
+const computerChoice = (imageCode) => {
+  /* rspCords 객체의 엔트리(키-쌍)를 배열 형태로 변환하여, Object.entries(rspCords)를 호출함
+   * 그 배열에서 find 메서드를 사용하여, 조건에 맞는 첫 번째 요소를 찾음
+   * find 메서드는 주어진 함수의 결과가 true인 첫 번째 요소를 반환
+   */
+  return Object.entries(rspCords).find(function (v) {
+    /* find 메서드에 전달된 함수에서, v는 배열의 각 요소(키-값 쌍)
+     * 여기서 v[1]는 값에 해당하며, 이 값이 imageCode와 같은이 비교
+     * 이 조건이 true일 경우, 해당 요소가 반환
+     */
+    return v[1] === imageCode;
+  })[0]; // find 메서드가 반환한 요소는 배열 형태로, [0]을 사용하여 그 배열의 첫 번째 요소(즉, 키)를 가져옴
+};
+
+let interval = null;
 export default {
   data() {
     return {
@@ -38,18 +57,38 @@ export default {
     },
   },
   methods: {
-    onClickButton(choice) {},
+    changeHand() {
+      interval = setInterval(() => {
+        if (this.imageCode === rspCords.바위) {
+          this.imageCode = rspCords.가위;
+        } else if (this.imageCode === rspCords.가위) {
+          this.imageCode = rspCords.보;
+        } else if (this.imageCode === rspCords.보) {
+          this.imageCode = rspCords.바위;
+        }
+      }, 100);
+    },
+    onClickButton(choice) {
+      clearInterval(interval);
+      const myScore = scores[choice];
+      const cpuScore = scores[computerChoice(this.imageCode)];
+      const diff = myScore - cpuScore;
+      if (diff === 0) {
+        this.result = "비겼습니다.";
+      } else if ([-1, 2].includes(diff)) {
+        this.result = "이겼습니다.";
+        this.score += 1;
+      } else {
+        this.result = "졌습니다.";
+        this.score -= 1;
+      }
+      setTimeout(() => {
+        this.changeHand()
+      }, 1000)
+    },
   },
   mounted() {
-    interval = setInterval(() => {
-      if (this.imageCode === rspCords.바위) {
-        this.imageCode = rspCords.가위;
-      } else if (this.imageCode === rspCords.가위) {
-        this.imageCode = rspCords.보;
-      } else if (this.imageCode === rspCords.보) {
-        this.imageCode = rspCords.바위;
-      }
-    }, 100);
+    this.changeHand();
   },
   beforeDestroy() {
     clearInterval(interval);
