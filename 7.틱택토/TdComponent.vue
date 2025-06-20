be4feb8de730d9280c1a821b7ecf0b4662a82558
NewBean0312 <newbean0312@gmail.com>
2025-06-20 23:28:56 +0900

    7.2차원배열(테이블 구조 짜기)

---
+++ 7.틱택토/TdComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <td>{{ cellData }}</td>
+</template>
+
+<script>
+export default {
+  props: {
+    cellData: String,
+    rowIndex: Number,
+    cellIndex: Number,
+  },
+};
+</script>
+
+<style></style>
