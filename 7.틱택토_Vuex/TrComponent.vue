2fbb68adf1905b736c2ff51c2ea18bfb56c5735b
NewBean0312 <newbean0312@gmail.com>
2025-06-29 23:45:50 +0900

    7.틱택토(Vuex 구조 세팅)

---
+++ 7.틱택토_Vuex/TrComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <tr>
+    <td-component
+      v-for="(cellData, index) in rowData"
+      :cell-data="cellData"
+      :cell-index="index"
+      :row-index="rowIndex"
+      :key="index"
+    ></td-component>
+  </tr>
+</template>
+
+<script>
+import TdComponent from "./TdComponent";
+
+export default {
+  components: {
+    TdComponent,
+  },
+  data() {
+    parent: "부모 컴포넌트";
+  },
+  props: {
+    rowData: Array,
+    rowIndex: Number,
+  },
+};
+</script>
+
+<style></style>
